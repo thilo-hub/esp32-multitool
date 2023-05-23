04138862eb0750f35d1e1638c4cc1729dcedba99
thilo jeremias <thilo@nispuk.com>
2023-05-23 21:45:07 +0200

    rf433 esp32 not needed anymorew

--- rf433/main/serio.h
+++
@@ -1,6 +0,0 @@
-void register_uart(void);
-
-
-int startUart(int,char **);
-
-// extern RingbufHandle_t wifiToSerial, serialToWifi;
--- rf433/main/version.h
+++
@@ -1,2 +0,0 @@
-#define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[f66ef6b85c0e432c2404082a08522a7628bb98fa, (HEAD -> main)]\r\n";
