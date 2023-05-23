040bca4e6df05fec55601cbc3431cc3030d8e258
thilo jeremias <thilo@nispuk.com>
2023-05-23 22:22:18 +0200

    version is autogenerated...

--- esptunnel/main/version.h
+++ esptunnel/main/version.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[ec33bd7856da6b0ada9f489c68f718c7a762d150, (HEAD -> main)]\r\n";
+ static const char* GIT_INFO = "Version Information=[04138862eb0750f35d1e1638c4cc1729dcedba99, (HEAD -> main)]\r\n";
