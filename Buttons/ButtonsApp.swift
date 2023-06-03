1f1a513bfe488c8d0e71e06a2fcbbdfd73ac56c6
thilo jeremias <thilo@nispuk.com>
2023-06-03 16:09:24 +0200

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Thilo Jeremias on 03.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
